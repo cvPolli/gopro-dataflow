e07cf53628caca2ec9faf5f5c4c560a1773867e5
arthur polli <arthur.polli@altave.com.br>
2024-02-06 11:13:01 -0300

    start work on export frames

--- gopro_dataflow/utils/frames.py
+++ gopro_dataflow/utils/frames.py
@@ -1,6 +1,6 @@
 import cv2
 
-def getTotal(videoPath):
+def get_total_frames(videoPath):
     try:
         cap = cv2.VideoCapture(videoPath)
         totalFrames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
@@ -8,6 +8,10 @@ def getTotal(videoPath):
     except Exception as ex:
         print(ex)        
     return totalFrames
+
+def export_frame_at_distance(video_path:str,video_data:list,distance_rate)->None:
+    cap = cv2.VideoCapture(video_path)
+    pass
 
 
 
